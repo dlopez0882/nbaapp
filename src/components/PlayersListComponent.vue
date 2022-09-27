d974f665aa1951dcc4a33c9f1c58264d34f7cf86
David Lopez <dlopez0882@gmail.com>
2022-09-27 11:07:14 -0700

    move axios logic to child component, restyle input, remove eslint package, add bootstrap

--- src/components/PlayersListComponent.vue
+++ src/components/PlayersListComponent.vue
@@ -1,65 +1,78 @@
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Position</th>
-        <th>Team</th>
-      </tr>
-    </thead>
-    <tr v-for="player in players" :key="player.id">
-      <td>{{ player.id }}</td>
-      <td>{{ player.first_name }}</td>
-      <td>{{ player.last_name }}</td>
-      <td>{{ player.position }}</td>
-      <td>{{ player.team.full_name }}</td>
-    </tr>
-  </table>
+    <div class="container">
+        <table class="table table-sm table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th class="table-text">ID</th>
+                    <th class="table-text">First name</th>
+                    <th class="table-text">Last name</th>
+                    <th class="table-text">Position</th>
+                    <th class="table-text">Team</th>
+                </tr>
+            </thead>
+            <tbody>
+            <tr v-for="player in players" :key="player.id">
+                <td class="table-text">{{ player.id }}</td>
+                <td class="table-text">{{ player.first_name }}</td>
+                <td class="table-text">{{ player.last_name }}</td>
+                <td class="table-text">{{ player.position }}</td>
+                <td class="table-text">{{ player.team.full_name }}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-  name: "PlayersListComponent",
+    name: "PlayersListComponent",
 
-  props: {
-    page: Number,
-    per_page: Number,
-    search: String,
-  },
+    props: {
+        page: Number,
+        per_page: Number,
+        search: String,
+    },
 
-  data() {
-    return {
-      players: {},
-    };
-  },
+    data() {
+        return {
+            players: {},
+        };
+    },
 
-  mounted() {
-    const headers = {
-      "X-RapidAPI-Key": "959819e95cmshecf23a99cc98e23p15b9d9jsn5e3fd589ab8a",
-      "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
-    };
+    mounted() {
+        const headers = {
+            "X-RapidAPI-Key": "959819e95cmshecf23a99cc98e23p15b9d9jsn5e3fd589ab8a",
+            "X-RapidAPI-Host": "free-nba.p.rapidapi.com",
+        };
 
-    axios
-      .get(
-        "https://free-nba.p.rapidapi.com/players?" +
-          "page=" +
-          this.page +
-          "&per_page=" +
-          this.per_page +
-          "&search=" +
-          this.search,
-        { headers }
-      )
-      .then((response) => {
-        this.players = response.data.data;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
+        axios
+            .get(
+                "https://free-nba.p.rapidapi.com/players?" +
+                "page=" +
+                this.page +
+                "&per_page=" +
+                this.per_page +
+                "&search=" +
+                this.search,
+                { headers }
+            )
+            .then((response) => {
+                this.players = response.data.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
 };
 </script>
+
+<style scoped>
+.row :nth-child(even){
+  background-color: #dcdcdc;
+}
+.row :nth-child(odd){
+  background-color: #aaaaaa;
+}
+</style>
--- src/views/PlayersView.vue
+++ src/views/PlayersView.vue
@@ -1,53 +1,69 @@
 <template>
-  <label for="per-page-option">Records per page: </label>
-  <select
-    name="per-page-option"
-    id="perpageoption"
-    @change="onChange($event)"
-    v-model="per_page"
-  >
-    <option value="25">25</option>
-    <option value="50">50</option>
-    <option value="75">75</option>
-    <option value="100">100</option>
-  </select>
+    <div class="container">
+        <div class="row mb-3">
+            <div class="col-md-4">
+                <label for="search">Player name: </label>
+                <input type="text" class="form-control" name="search" @blur="queryByName($event)"
+                    @keyup.enter="queryByName($event)" />
+            </div>
 
-  <label for="search">Player name: </label>
-  <input type="text" name="search" @blur="onBlur($event)" />
-  <PlayersListComponent
-    :key="reload"
-    :page="page"
-    :per_page="per_page"
-    :search="search"
-  />
+            <div class="col-md-4">
+                <label for="per-page-option">Records per page: </label>
+                <select name="per-page-option" id="perpageoption" class="form-control" @change="onChange($event)"
+                    v-model="per_page">
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                    <option value="75">75</option>
+                    <option value="100">100</option>
+                </select>
+            </div>
+
+            <div class="col-md-4">
+                <label for="page">Page: </label>
+                <input type="text" class="form-control" name="page" @blur="queryByPage($event)"
+                    @keyup.enter="queryByPage($event)" />
+            </div>
+        </div>
+    </div>
+
+    <PlayersListComponent :key="reload" :page="page" :per_page="per_page" :search="search" />
 </template>
 
 <script>
 import PlayersListComponent from "@/components/PlayersListComponent.vue";
 
 export default {
-  name: "PlayersView",
-  components: { PlayersListComponent },
+    name: "PlayersView",
+    components: { PlayersListComponent },
 
-  data() {
-    return {
-      page: "0",
-      per_page: "25",
-      search: "",
-      reload: 0,
-    };
-  },
-
-  methods: {
-    onChange(event) {
-      this.per_page = event.target.value;
-      this.reload++;
+    data() {
+        return {
+            page: "0",
+            per_page: "25",
+            search: "",
+            reload: 0,
+        };
     },
 
-    onBlur(event) {
-      this.search = event.target.value;
-      this.reload++;
+    methods: {
+        onChange(event) {
+            this.per_page = event.target.value;
+            this.reload++;
+        },
+
+        queryByName(event) {
+            this.search = event.target.value;
+            if (this.search.length > 0) {
+                this.reload++;
+            }
+        },
+
+        queryByPage(event) {
+            this.page = event.target.value;
+            if (this.page.length > 0) {
+                this.reload++;
+            }
+        },
     },
-  },
 };
 </script>
