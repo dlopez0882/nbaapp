4bd4cf9bca65a551176b816ad421efc1caba76ad
David Lopez <dlopez0882@gmail.com>
2022-09-28 22:47:45 -0700

    add function to formatted date returned from API call

--- src/components/GamesInfoComponent.vue
+++ src/components/GamesInfoComponent.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div id="gameInfo" class="card mb-3" v-for="dataPoint in gameData" :key="dataPoint.id">
             <div class="card-body">
-                <p>Date: {{ dataPoint.date }}</p>
+                <p>Date: {{ dateFormatter(dataPoint.date) }}</p>
                 <p>Matchup: {{ dataPoint.visitor_team.full_name }} at {{ dataPoint.home_team.full_name }}</p>
                 <p>Score: {{ dataPoint.visitor_team.abbreviation }} {{ dataPoint.visitor_team_score }} | {{ dataPoint.home_team.abbreviation }} {{ dataPoint.home_team_score }} ({{ dataPoint.status }})</p>
             </div>
@@ -34,6 +34,13 @@ import axios from 'axios';
                 .catch(error => {
                     console.log(error);
                 })
+        },
+
+        methods: {
+            dateFormatter(timestamp) {
+                let d = new Date(timestamp);
+                return d.toDateString();
+            }
         }
     };
 </script>
