894dff705e011f28182c0528b487f08ef472a93e
David Lopez <dlopez0882@gmail.com>
2022-10-04 15:21:52 -0700

    set data function to current expected data formats

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -44,10 +44,10 @@
 
         data() {
             return {
-                teams: {},
-                selectedTeam: "",
+                teams: [], // data returned from /teams API that populates Team selection menu
+                selectedTeam: "", // data to be passed to GamesInfoComponent prop "team_ids"
                 seasonOptions: ["2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"],
-                season: "",
+                season: [],
                 reload: 0,
             };
         },
