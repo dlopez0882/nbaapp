143e5895971d066d67b00ee332698d9ab5154d50
David Lopez <dlopez0882@gmail.com>
2022-10-07 10:19:27 -0700

    add logic change displayed names and abbreviations for legacy teams

--- src/components/GamesInfoComponent.vue
+++ src/components/GamesInfoComponent.vue
@@ -60,6 +60,7 @@ import axios from 'axios';
                 axios.get("https://free-nba.p.rapidapi.com/games?seasons[]=" + this.season + "&team_ids[]=" + this.team_ids + "&per_page=100&page=1",{ headers })
                     .then(response => {
                         this.gameData = response.data.data;
+                        this.transformName();
                     })
                     .catch(error => {
                         console.log(error);
@@ -75,6 +76,87 @@ import axios from 'axios';
                 let d = new Date(timestamp);
                 return d.toDateString();
             },
+
+            transformName() {
+                // Seattle SuperSonics
+                if(this.season < 2008) {
+                    this.gameData.forEach((item) => {
+                        item.home_team.full_name = (item.home_team.abbreviation == "OKC") ? "Seattle SuperSonics" : item.home_team.full_name;
+                        item.visitor_team.full_name = (item.visitor_team.abbreviation == "OKC") ? "Seattle SuperSonics" : item.visitor_team.full_name;
+                        item.home_team.abbreviation = (item.home_team.abbreviation == "OKC") ? "SEA" : item.home_team.abbreviation;
+                        item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "OKC") ? "SEA" : item.visitor_team.abbreviation;
+                    })
+                }
+
+                // San Diego Clippers
+                if(this.season < 1984) {
+                    this.gameData.forEach((item) => {
+                        item.home_team.full_name = (item.home_team.abbreviation == "LAC") ? "San Diego Clippers" : item.home_team.full_name;
+                        item.visitor_team.full_name = (item.visitor_team.abbreviation == "LAC") ? "San Diego Clippers" : item.visitor_team.full_name;
+                        item.home_team.abbreviation = (item.home_team.abbreviation == "LAC") ? "SDC" : item.home_team.abbreviation;
+                        item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "LAC") ? "SDC" : item.visitor_team.abbreviation;
+                    })
+                }
+
+                // Kansas City Kings
+                if(this.season < 1985) {
+                    this.gameData.forEach((item) => {
+                        item.home_team.full_name = (item.home_team.abbreviation == "SAC") ? "Kansas City Kings" : item.home_team.full_name;
+                        item.visitor_team.full_name = (item.visitor_team.abbreviation == "SAC") ? "Kansas City Kings" : item.visitor_team.full_name;
+                        item.home_team.abbreviation = (item.home_team.abbreviation == "SAC") ? "KCK" : item.home_team.abbreviation;
+                        item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "SAC") ? "KCK" : item.visitor_team.abbreviation;
+                    })
+                }
+
+                // Vancouver Grizzlies
+                if(this.season < 2001) {
+                    this.gameData.forEach((item) => {
+                        item.home_team.full_name = (item.home_team.abbreviation == "MEM") ? "Vancouver Grizzlies" : item.home_team.full_name;
+                        item.visitor_team.full_name = (item.visitor_team.abbreviation == "MEM") ? "Vancouver Grizzlies" : item.visitor_team.full_name;
+                        item.home_team.abbreviation = (item.home_team.abbreviation == "MEM") ? "VAN" : item.home_team.abbreviation;
+                        item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "MEM") ? "VAN" : item.visitor_team.abbreviation;
+                    })
+                }
+
+                // New Jersey Nets
+                if(this.season < 2012) {
+                    this.gameData.forEach((item) => {
+                        item.home_team.full_name = (item.home_team.abbreviation == "BKN") ? "New Jersey Nets" : item.home_team.full_name;
+                        item.visitor_team.full_name = (item.visitor_team.abbreviation == "BKN") ? "New Jersey Nets" : item.visitor_team.full_name;
+                        item.home_team.abbreviation = (item.home_team.abbreviation == "BKN") ? "NJN" : item.home_team.abbreviation;
+                        item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "BKN") ? "NJN" : item.visitor_team.abbreviation;
+                    })
+                }
+
+                // New Orleans Hornets
+                if(this.season < 2013) {
+                    // New Orleans/Oklahoma City Hornets
+                    if(this.season === 2005 || this.season === 2006) {
+                        this.gameData.forEach((item) => {
+                            item.home_team.full_name = (item.home_team.abbreviation == "NOP") ? "New Orleans/Oklahoma City Hornets" : item.home_team.full_name;
+                            item.visitor_team.full_name = (item.visitor_team.abbreviation == "NOP") ? "New Orleans/Oklahoma City Hornets" : item.visitor_team.full_name;
+                            item.home_team.abbreviation = (item.home_team.abbreviation == "NOP") ? "NOK" : item.home_team.abbreviation;
+                            item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "NOP") ? "NOK" : item.visitor_team.abbreviation;
+                        })
+                    } else {
+                        // New Orleans Hornets
+                        this.gameData.forEach((item) => {
+                            item.home_team.full_name = (item.home_team.abbreviation == "NOP") ? "New Orleans Hornets" : item.home_team.full_name;
+                            item.visitor_team.full_name = (item.visitor_team.abbreviation == "NOP") ? "New Orleans Hornets" : item.visitor_team.full_name;
+                            item.home_team.abbreviation = (item.home_team.abbreviation == "NOP") ? "NOH" : item.home_team.abbreviation;
+                            item.visitor_team.abbreviation = (item.visitor_team.abbreviation == "NOP") ? "NOH" : item.visitor_team.abbreviation;
+                        })
+                    }
+                }
+
+                // Charlotte Bobcats
+                if(this.season > 2004 && this.season < 2014) {
+                    this.gameData.forEach((item) => {
+                        item.home_team.full_name = (item.home_team.abbreviation == "CHA") ? "Charlotte Bobcats" : item.home_team.full_name;
+                        item.visitor_team.full_name = (item.visitor_team.abbreviation == "CHA") ? "Charlotte Bobcats" : item.visitor_team.full_name;
+                    })
+                }
+            }
 
             // Leaving here in case a potential solution for sorting API data becomes available in future
             // format params that need to be passed as an array into API 
