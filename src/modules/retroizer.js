1a792914cdfd32917bb3818e6527efab202e6d05
David Lopez <dlopez0882@gmail.com>
2022-10-13 22:28:35 -0700

    use switch statement instead

--- src/modules/retroizer.js
+++ src/modules/retroizer.js
@@ -1,83 +1,51 @@
 // a utility to retro team names
 export const nameRetroizer = function(season, team) {
-    // Seattle SuperSonics
-    if(team == "Oklahoma City Thunder") {
-        return (season < 2008) ? "Seattle SuperSonics" : team;
+    switch(team) {
+        case "Oklahoma City Thunder":
+            return (season < 2008) ? "Seattle SuperSonics" : team;
+        case "LA Clippers":
+            return (season < 1984) ? "San Diego Clippers" : team;
+        case "Sacramento Kings":
+            return (season < 1985) ? "Kansas City Kings" : team;
+        case "Memphis Grizzlies":
+            return (season < 2001) ? "Vancouver Grizzlies" : team;
+        case "Brooklyn Nets":
+            return (season < 2012) ? "New Jersey Nets" : team;
+        case "Charlotte Hornets":
+            return (season > 2004 && season < 2014) ? "Charlotte Bobcats" : team;
+        case "New Orleans Pelicans":
+            if (season === 2005 || season === 2006) {
+                return "New Orleans/Oklahoma City Hornets";
+            } else if (season < 2013) {
+                return "New Orleans Hornets"
+            }
+        default:
+            return team;
     }
-
-    // San Diego Clippers
-    if(team == "LA Clippers") {
-        return (season < 1984) ? "San Diego Clippers" : team;
-    }
-
-    // Kansas City Kings
-    if(team == "Sacramento Kings") {
-        return (season < 1985) ? "Kansas City Kings" : team;
-    }
-
-    // Vancouver Grizzlies
-    if(team == "Memphis Grizzlies") {
-        return (season < 2001) ? "Vancouver Grizzlies" : team;
-    }
-
-    // New Jersey Nets
-    if(team == "Brooklyn Nets") {
-        return (season < 2012) ? "New Jersey Nets" : team;
-    }
-
-    // New Orleans/Oklahoma City Hornets
-    if(team == "New Orleans Pelicans" && (season === 2005 || season === 2006)) {
-        return "New Orleans/Oklahoma City Hornets";
-    // New Orleans Hornets
-    } else if (team == "New Orleans Pelicans" && season < 2013) {
-        return "New Orleans Hornets";
-    }
-
-    // Charlotte Bobcats
-    if(team == "Charlotte Hornets") {
-        return (season > 2004 && season < 2014) ? "Charlotte Bobcats" : team;
-    }
-
-    // return incoming team if it doesn't fall into any of the conditions above...
-    return team;
 }
 
 
 // a utility to retro team abbreviations
 export const abbreviationRetroizer = function(season, team)  {
-    // Seattle SuperSonics
-    if(team == "OKC") {
-        return (season < 2008) ? "SEA" : team;
+    switch(team) {
+        case "OKC":
+            return (season < 2008) ? "SEA" : team;
+        case "LAC": 
+            return (season < 1984) ? "SDC" : team;
+        case "SAC":
+            return (season < 1985) ? "KCK" : team;
+        case "MEM":
+            return (season < 2001) ? "VAN" : team;
+        case "BKN":
+            return (season < 2012) ? "NJN" : team;
+        case "NOP":
+            if (season === 2005 || season === 2006) {
+                return "NOK";
+            } else if (season < 2013) {
+                return "NOH";
+            }
+        default: 
+            return team;
     }
 
-    // San Diego Clippers
-    if(team == "LAC") {
-        return (season < 1984) ? "SDC" : team;
-    }
-
-    // Kansas City Kings
-    if(team == "SAC") {
-        return (season < 1985) ? "KCK" : team;
-    }
-
-    // Vancouver Grizzlies
-    if(team == "MEM") {
-        return (season < 2001) ? "VAN" : team;
-    }
-
-    // New Jersey Nets
-    if(team == "BKN") {
-        return (season < 2012) ? "NJN" : team;
-    }
-
-    // New Orleans/Oklahoma City Hornets
-    if(team == "NOP" && (season === 2005 || season === 2006)) {
-        return "NOK";
-    // New Orleans Hornets
-    } else if (team == "NOP" && season < 2013) {
-        return "NOH";
-    }
-
-    // return incoming team if it doesn't fall into any of the conditions above...
-    return team;
 }
