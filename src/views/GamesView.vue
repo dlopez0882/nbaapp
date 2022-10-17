dfaa86941c618ce27561b4b7816ed547e09066b4
David Lopez <dlopez0882@gmail.com>
2022-10-17 01:34:45 -0700

    display blurb for historical teams, and removed function that modifies existing team dropdown

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -5,16 +5,17 @@
                 <div class="row row justify-content-around">
                     <div class="form-group col-md-4 mb-3">
                         <label for="teams">Team:</label>
-                        <select name="teams" id="teams" class="form-control" aria-label="select team" v-model="selectedTeam">
+                        <select name="teams" id="teams" class="form-control" aria-label="select team" v-model="selectedTeam" @change="toggleTeamHintText()">
                             <option v-if="displayLoadingOption" value="" selected>-- Loading... --</option>
                             <option v-else value="" selected>-- Select team --</option>
                             <option v-for="team in teams" :key="team.id" :value="team.id">{{ team.full_name }}</option>
                         </select>
+                        <small v-if="displayTeamHintText">{{ teamHintText }}</small>
                     </div>
 
                     <div class="form-group col-md-4 mb-3">
                         <label for="teams">Season:</label>
-                        <select name="seasons" id="seasons" class="form-control" aria-label="select season" v-model="selectedSeason">
+                        <select name="seasons" id="seasons" class="form-control" aria-label="select season" v-model="selectedSeason" @change="toggleTeamHintText()">
                             <option value="" selected>-- Select season --</option>
                             <option v-for="season in seasonOptions" :value="season.value">{{ season.displayValue }}</option>
                         </select>
@@ -73,6 +74,8 @@
                 selectedSeason: "", // data to be passed to GamesInfoComponent prop "season"
                 reload: 0, // aids in reloading component when submit is clicked
                 displayLoadingOption: true,
+                displayTeamHintText: false,
+                teamHintText: "",
             };
         },
 
@@ -86,7 +89,6 @@
                 .then(response => {
                     // console.log(response.data.data);
                     this.teams = response.data.data;
-                    this.reviseNames();
                 })
                 .catch(error => {
                     console.log(error);
@@ -111,32 +113,50 @@
                 this.reload++;
             },
 
-            // display historical team names since API only returns current team names.
-            // ex. Seattle Supersonics --> Oklahoma City Thunder
-            reviseNames() {
-                const okcindex = this.teams.findIndex(team => team.abbreviation == "OKC");
-                this.teams[okcindex].full_name = "Oklahoma City Thunder (Seattle SuperSonics)";
+            // displays a blurb about historical team name based on selected team and season
+            toggleTeamHintText() {
+                const okc = this.teams.find(team => team.abbreviation == "OKC");
+                const lac = this.teams.find(team => team.abbreviation == "LAC");
+                const sac = this.teams.find(team => team.abbreviation == "SAC");
+                const mem = this.teams.find(team => team.abbreviation == "MEM");
+                const bkn = this.teams.find(team => team.abbreviation == "BKN");
+                const nop = this.teams.find(team => team.abbreviation == "NOP");
+                const cha = this.teams.find(team => team.abbreviation == "CHA");
+                const was = this.teams.find(team => team.abbreviation == "WAS");
 
-                const lacindex = this.teams.findIndex(team => team.abbreviation == "LAC");
-                this.teams[lacindex].full_name = "Los Angeles (San Diego) Clippers";
-
-                const sacindex = this.teams.findIndex(team => team.abbreviation == "SAC");
-                this.teams[sacindex].full_name = "Sacramento (Kansas City) Kings";
-
-                const memindex = this.teams.findIndex(team => team.abbreviation == "MEM");
-                this.teams[memindex].full_name = "Memphis (Vancouver) Grizzlies";
-
-                const bknindex = this.teams.findIndex(team => team.abbreviation == "BKN");
-                this.teams[bknindex].full_name = "Brooklyn (New Jersey) Nets";
-
-                const nopindex = this.teams.findIndex(team => team.abbreviation == "NOP");
-                this.teams[nopindex].full_name = "New Orleans Pelicans (Hornets)";
-
-                const chaindex = this.teams.findIndex(team => team.abbreviation == "CHA");
-                this.teams[chaindex].full_name = "Charlotte Hornets (Bobcats)";
-
-                const wasindex = this.teams.findIndex(team => team.abbreviation == "WAS");
-                this.teams[wasindex].full_name = "Washington Wizards (Bullets)";
+                if(this.selectedTeam && this.selectedSeason) {
+                    if(this.selectedTeam == okc.id && this.selectedSeason < 2008) {
+                        this.teamHintText = "Seattle SuperSonics prior to the 2008-2009 season.";
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == lac.id && this.selectedSeason < 1984) {
+                        this.teamHintText = "San Diego Clippers prior to 1984-1985 season.";
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == sac.id && this.selectedSeason < 1985) {
+                        this.teamHintText = "Kansas City Kings prior to the 1985-1986 season.";
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == mem.id && this.selectedSeason < 2001) {
+                        this.teamHintText = "Vancouver Grizzlies prior to the 2001-2002 season.";
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == bkn.id && this.selectedSeason < 2012) {
+                        this.teamHintText = "New Jersey Nets prior to the 2012-2013 season.";
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == nop.id && this.selectedSeason < 2013) {
+                        if(this.selectedSeason == 2005 || this.selectedSeason == 2006) {
+                            this.teamHintText = "New Orleans/Oklahoma City Hornets for the 2005 and 2006 seasons.";
+                        } else {
+                            this.teamHintText = "New Orleans Hornets prior to the 2013-2014 season.";
+                        }
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == cha.id && this.selectedSeason > 2004 && this.selectedSeason < 2014) {
+                        this.teamHintText = "Charlotte Bobcats between the 2005-2006 and 2013-2014 seasons.";
+                        this.displayTeamHintText = true;
+                    } else if (this.selectedTeam == was.id && this.selectedSeason < 1997) {
+                        this.teamHintText = "Washington Bullets prior to the 1997-1998 season.";
+                        this.displayTeamHintText = true;
+                    } else {
+                        this.displayTeamHintText = false;
+                    }
+                }
             },
 
             // Leaving here in case a potential solution for sorting API data becomes available in future
