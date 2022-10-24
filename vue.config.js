ecd6cc00f68865d750a617e1b5ef954786b45e67
David Lopez <dlopez0882@gmail.com>
2022-10-24 12:14:04 -0700

    change publicPath to repo name if running in production environment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    publicPath: '/nbaapp/',
+    // publicPath: '/nbaapp/',
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/nbaapp/' // note the trailing slash
+        : '/',
     transpileDependencies: true,
 });
