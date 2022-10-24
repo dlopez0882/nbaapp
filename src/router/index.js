b83acb2c51df9216d029effc12fb6fab46b87c6f
David Lopez <dlopez0882@gmail.com>
2022-10-24 12:09:24 -0700

    change vue-router history settings for app

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+//   history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/nbaapp/'),
   routes,
 });
 
