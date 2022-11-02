01905d33cca62c6635d403c5770fd0f4f7e56ad6
David Lopez <dlopez0882@gmail.com>
2022-11-02 14:55:08 -0700

    clean up

--- src/components/GamesTabPaneComponent.vue
+++ src/components/GamesTabPaneComponent.vue
@@ -1,29 +1,8 @@
 <template>
     <ul class="nav nav-tabs" id="myTab" role="tablist">
-        <!-- <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="regular-season-completed-tab" data-bs-toggle="tab" data-bs-target="#regular-season-completed" type="button" role="tab" aria-controls="regular-season-completed" aria-selected="true">
-                <span v-if="regularSeasonUpcomingData.length > 0">Regular Season - Completed</span>
-                <span v-else>Regular Season</span>
-            </button>
-        </li>
-        <li v-if="regularSeasonUpcomingData.length > 0" class="nav-item" role="presentation">
-            <button class="nav-link" id="regular-season-upcoming-tab" data-bs-toggle="tab" data-bs-target="#regular-season-upcoming" type="button" role="tab" aria-controls="regular-season-upcoming" aria-selected="false">Regular Season - Upcoming</button>
-        </li>
-        <li class="nav-item" role="presentation">
-            <button class="nav-link" id="playoffs-completed-tab" data-bs-toggle="tab" data-bs-target="#playoffs-completed" type="button" role="tab" aria-controls="playoffs-completed" aria-selected="false">
-                <span v-if="postSeasonUpcomingData.length > 0">Playoffs - Completed</span>
-                <span v-else>Playoffs</span>
-            </button>
-        </li>
-        <li v-if="postSeasonUpcomingData.length > 0" class="nav-item" role="presentation">
-            <button class="nav-link" id="playoffs-upcoming-tab" data-bs-toggle="tab" data-bs-target="#playoffs-upcoming" type="button" role="tab" aria-controls="playoffs-upcoming" aria-selected="false">Playoffs - Upcoming</button>
-        </li> -->
-
         <li v-for="tab in tabs" class="nav-item" role="presentation">
             <button v-if="tab.displayButton" :class="tab.buttonClass" :id="tab.buttonId" data-bs-toggle="tab" :data-bs-target="tab.dataBsTarget" type="button" role="tab" :aria-controls="tab.ariaControls" :aria-selected="tab.ariaSelected">
                 <span>{{ tab.buttonLabel }}</span>
-                <!-- <span v-if="regularSeasonUpcomingData.length > 0">Regular Season - Completed</span>
-                <span v-else>Regular Season</span> -->
             </button>
         </li>
     </ul>
