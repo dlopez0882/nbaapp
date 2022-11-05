dd46701f80fe477864ba4d9784e40d3edb11e949
David Lopez <dlopez0882@gmail.com>
2022-11-05 10:38:28 -0700

    add logic to display overtime 'OT' status (#20)
    * display "OT" next to game status if a game ends in overtime.  
    
    * will display "OT" for a single overtime period. thereafter, will display an increment on how many times overtime is repeated (i.e. 2OT, 3OT, etc).

--- src/components/GameCardComponent.vue
+++ src/components/GameCardComponent.vue
@@ -6,7 +6,7 @@
         </div>
         <div v-else class="card-body">
             <p>Date: {{ data.date }}</p>
-            <p v-html="highlightWinner(data.visitor_team.abbreviation, data.visitor_team_score, data.home_team.abbreviation, data.home_team_score, data.status, data.time)"></p>
+            <p v-html="highlightWinner(data.visitor_team.abbreviation, data.visitor_team_score, data.home_team.abbreviation, data.home_team_score, this.checkIfOvertimeStatus(data.period, data.status), data.time)"></p>
             <p><a href="javascript:void(0)" @click="this.$parent.showStatsModal(data.id, data.visitor_team.abbreviation, data.home_team.abbreviation, data.date)" @keydown.esc="this.$parent.hideStatsModal()">View game stats</a></p>
         </div>
     </div>
@@ -23,11 +23,20 @@
         methods: {
             // highlights the winning team's score if game status is finalized
             highlightWinner(visitorTeam, visitorScore, homeTeam, homeScore, gameStatus, gameTime) {
-                return (gameStatus.toUpperCase() === 'FINAL')  ? 
+                return (gameStatus.toUpperCase().includes('FINAL'))  ? 
                     (visitorScore > homeScore) ?
                         `Score: <strong>` + visitorTeam + ` ` + visitorScore + `</strong> | ` + homeTeam + ` ` + homeScore + ` (` + gameStatus + `)` : 
                         `Score: ` + visitorTeam + ` ` + visitorScore + ` | <strong>` + homeTeam + ` ` + homeScore + `</strong> (` + gameStatus + `)`
                     : visitorTeam + ` ` + visitorScore + ` | ` + homeTeam + ` ` + homeScore + ` (` + gameTime + `)`
+            },
+
+            checkIfOvertimeStatus(period, status) {
+                let overtimeCount = period-4;
+                // proper OT report is "Final OT", "Final 2OT", "Final 3OT", etc.
+                status = (overtimeCount > 0) ? 
+                    (overtimeCount == 1) ? status + ' OT' : status + ' ' + overtimeCount + 'OT' :
+                    status;
+                return status;
             },
         }
     }
